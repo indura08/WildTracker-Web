872cdb3a1026401649059cb784e43af53e21b03b
Hirusha Dinujaya <74463840+hdx0315@users.noreply.github.com>
2025-02-08 23:40:48 +0530

    .env

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,29 +1,22 @@
-
-// wildtracker-web/firebase/config.ts
-
 // Import the functions you need from the SDKs you need
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDlrXe3fnMsoHAnkdaqvpCMxoLLavQa0LY",
-  authDomain: "wildtracker-438e0.firebaseapp.com",
-  projectId: "wildtracker-438e0",
-  storageBucket: "wildtracker-438e0.firebasestorage.app",
-  messagingSenderId: "103186647108",
-  appId: "1:103186647108:web:693f36f2d11cedfb1c5411"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 
 export const firestore = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
-
